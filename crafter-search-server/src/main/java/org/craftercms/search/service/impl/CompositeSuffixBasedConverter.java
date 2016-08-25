c4bc3860830d904ab2253043fb8e39bb45193ad7
Alfonso Vásquez <avasquez@rivetlogic.com>
2016-08-25 11:14:35 -0600

    Fix for CRAFTERCMS-2070.

--- crafter-search-server/src/main/java/org/craftercms/search/service/impl/CompositeSuffixBasedConverter.java
+++ crafter-search-server/src/main/java/org/craftercms/search/service/impl/CompositeSuffixBasedConverter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2007-2016 Crafter Software Corporation.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.craftercms.search.service.impl;
 
 import java.util.Map;
@@ -7,7 +23,7 @@ import org.craftercms.search.service.FieldValueConverter;
 
 /**
  * Based on the suffix of a field, picks an actual {@link FieldValueConverter} from a set of suffix -> converter
- * mappings. For example, if a field ends with _dt, this converter can delegate to a {@link DateTimeConverter} to
+ * mappings. For example, if a field ends with _dt, this converter can delegate to a {@link IsoDateTimeConverter} to
  * convert the field.
  *
  * @author avasquez
