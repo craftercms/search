5b74147ab0a3168da8f2fc8acc9afd3670d1081b
Matt Wittmann <matt.wittmann@rivetlogic.com>
2017-02-13 13:32:43 -0600

    Fixing compiler errors after rebase which brought in Solr 6.4 refactorings

--- crafter-search-server-elasticsearch/src/main/java/org/craftercms/search/service/impl/elasticsearch/ElasticsearchController.java
+++ crafter-search-server-elasticsearch/src/main/java/org/craftercms/search/service/impl/elasticsearch/ElasticsearchController.java
@@ -1,22 +1,35 @@
+/*
+ * Copyright (C) 2007-2013 Crafter Software Corporation.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.craftercms.search.service.impl.elasticsearch;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.craftercms.search.exception.SearchException;
 import org.craftercms.search.service.SearchService;
 import org.craftercms.search.service.impl.QueryParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -24,18 +37,19 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.craftercms.search.service.SearchRestConstants.*;
+import static org.craftercms.search.rest.SearchRestApiConstants.*;
 
 /**
- * Copied from {@link org.craftercms.search.controller.SearchRestController} to be used in new Elasticsearch module.
+ * Copied from {@link org.craftercms.search.rest.controller.SearchRestController} to be used in new Elasticsearch module.
  */
-@Controller
+@RestController
 @RequestMapping(URL_ROOT)
 public class ElasticsearchController {
+
     private static final Logger logger = LoggerFactory.getLogger(ElasticsearchController.class);
 
     private static final String[] NON_ADDITIONAL_FIELD_NAMES =
-            {REQUEST_PARAM_INDEX_ID, REQUEST_PARAM_SITE, REQUEST_PARAM_ID, REQUEST_PARAM_DOCUMENT};
+            {PARAM_INDEX_ID, PARAM_SITE, PARAM_ID, PARAM_DOCUMENT};
 
     private final SearchService searchService;
 
@@ -45,43 +59,38 @@ public class ElasticsearchController {
     }
 
     @RequestMapping(value = URL_SEARCH, method = RequestMethod.GET)
-    @ResponseBody
-    public Map<String, Object> search(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId,
+    public Map<String, Object> search(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId,
                                       HttpServletRequest request) throws SearchException {
         return searchService.search(indexId, new QueryParams(request.getParameterMap()));
     }
 
     @RequestMapping(value = URL_UPDATE, method = RequestMethod.POST)
-    @ResponseBody
-    public String update(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId,
-                         @RequestParam(REQUEST_PARAM_SITE) String site,
-                         @RequestParam(REQUEST_PARAM_ID) String id,
-                         @RequestParam(REQUEST_PARAM_IGNORE_ROOT_IN_FIELD_NAMES) boolean ignoreRootInFieldNames,
+    public String update(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId,
+                         @RequestParam(PARAM_SITE) String site,
+                         @RequestParam(PARAM_ID) String id,
+                         @RequestParam(PARAM_IGNORE_ROOT_IN_FIELD_NAMES) boolean ignoreRootInFieldNames,
                          @RequestBody String xml) throws SearchException {
         return searchService.update(indexId, site, id, xml, ignoreRootInFieldNames);
     }
 
     @RequestMapping(value = URL_DELETE, method = RequestMethod.POST)
-    @ResponseBody
-    public String delete(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId,
-                         @RequestParam(REQUEST_PARAM_SITE) String site,
-                         @RequestParam(value = REQUEST_PARAM_ID) String id) throws SearchException {
+    public String delete(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId,
+                         @RequestParam(PARAM_SITE) String site,
+                         @RequestParam(value = PARAM_ID) String id) throws SearchException {
         return searchService.delete(indexId, site, id);
     }
 
     @RequestMapping(value = URL_COMMIT, method = RequestMethod.POST)
-    @ResponseBody
-    public String commit(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId)
+    public String commit(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId)
             throws SearchException {
         return searchService.commit(indexId);
     }
 
     @RequestMapping(value = {URL_UPDATE_DOCUMENT, URL_PARTIAL_DOCUMENT_UPDATE}, method = RequestMethod.POST)
-    @ResponseBody
     @Deprecated
-    public String updateDocument(@RequestPart(REQUEST_PARAM_SITE) String site,
-                                 @RequestPart(REQUEST_PARAM_ID) String id,
-                                 @RequestPart(REQUEST_PARAM_DOCUMENT) MultipartFile document,
+    public String updateDocument(@RequestPart(PARAM_SITE) String site,
+                                 @RequestPart(PARAM_ID) String id,
+                                 @RequestPart(PARAM_DOCUMENT) MultipartFile document,
                                  HttpServletRequest request) throws SearchException {
         try {
             File tmpFile = File.createTempFile("crafter-" + document.getOriginalFilename(), "");
@@ -100,11 +109,10 @@ public class ElasticsearchController {
     }
 
     @RequestMapping(value = URL_UPDATE_FILE, method = RequestMethod.POST)
-    @ResponseBody
-    public String updateFile(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId,
-                             @RequestPart(REQUEST_PARAM_SITE) String site,
-                             @RequestPart(REQUEST_PARAM_ID) String id,
-                             @RequestPart(REQUEST_PARAM_FILE) MultipartFile file,
+    public String updateFile(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId,
+                             @RequestPart(PARAM_SITE) String site,
+                             @RequestPart(PARAM_ID) String id,
+                             @RequestPart(PARAM_FILE) MultipartFile file,
                              HttpServletRequest request) throws SearchException {
 
         try {
@@ -121,14 +129,6 @@ public class ElasticsearchController {
         } catch (IOException e) {
             throw new SearchException(e.getMessage(), e);
         }
-    }
-
-    @ExceptionHandler(Exception.class)
-    public void handleException(Exception e, HttpServletRequest request,
-                                HttpServletResponse response) throws IOException {
-        logger.error("RESTful request " + request.getRequestURI() + " failed", e);
-
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, ExceptionUtils.getRootCauseMessage(e));
     }
 
     protected Map<String, String> getAdditionalFields(HttpServletRequest request) {
@@ -156,4 +156,5 @@ public class ElasticsearchController {
 
         return additionalFields;
     }
+
 }
