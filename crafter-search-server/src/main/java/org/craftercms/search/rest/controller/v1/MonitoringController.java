df4795d54d592615d0a386a2f110259b3eb23fcf
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2017-04-05 15:54:07 -0400

    Added back old search API v1 to server. Now the server can both accept API v1 and v2 requests.

--- crafter-search-server/src/main/java/org/craftercms/search/rest/controller/v1/MonitoringController.java
+++ crafter-search-server/src/main/java/org/craftercms/search/rest/controller/v1/MonitoringController.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.craftercms.search.rest.controller;
+package org.craftercms.search.rest.controller.v1;
 
 import java.util.Collections;
 import java.util.Map;
--- crafter-search-server/src/main/java/org/craftercms/search/rest/controller/v2/SearchRestController.java
+++ crafter-search-server/src/main/java/org/craftercms/search/rest/controller/v2/SearchRestController.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.craftercms.search.rest.controller;
+package org.craftercms.search.rest.controller.v2;
 
 import java.io.File;
 import java.io.IOException;
