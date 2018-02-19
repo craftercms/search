136cfaae0ace99d43f266fd576b6b952cd3c9614
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2018-02-19 16:05:03 -0500

    Added API v1 Search Client:
    * Added a search client implementation that can call REST API v1 (compatible with 2.4 and 2.5 search servers)
    * Moved current REST Search Client to v2 package, so that we can keep adding new clients if a new API version is added and mantain v1 and v2 for compatibility.

--- crafter-search-api/src/main/java/org/craftercms/search/rest/v1/SearchRestApiConstants.java
+++ crafter-search-api/src/main/java/org/craftercms/search/rest/v1/SearchRestApiConstants.java
@@ -14,10 +14,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.craftercms.search.rest;
+package org.craftercms.search.rest.v1;
 
 /**
- * Search API REST constants used by both server and client.
+ * Search API REST constants used by both server and client (API v1).
  *
  * @author Alfonso Vásquez
  * @author Dejan Brkic
@@ -27,18 +27,19 @@ public class SearchRestApiConstants {
     private SearchRestApiConstants() {
     }
 
-    public static final String PARAM_INDEX_ID = "index_id";
-
-    public static final String PARAM_SITE = "site";
-    public static final String PARAM_ID = "id";
-    public static final String PARAM_IGNORE_ROOT_IN_FIELD_NAMES = "strip_root";
-    public static final String PARAM_CONTENT = "content";
-
-    public static final String URL_ROOT = "/api/2/search";
+    public static final String URL_ROOT = "/api/1/search";
     public static final String URL_SEARCH = "/search";
     public static final String URL_UPDATE = "/update";
-    public static final String URL_UPDATE_CONTENT = "/update-content";
+    public static final String URL_UPDATE_DOCUMENT = "/update-document";
+    public static final String URL_PARTIAL_DOCUMENT_UPDATE = "/partial-document-update";
+    public static final String URL_UPDATE_FILE= "/update-file";
     public static final String URL_DELETE = "/delete";
     public static final String URL_COMMIT = "/commit";
+
+    public static final String PARAM_INDEX_ID = "indexId";
+    public static final String PARAM_SITE = "site";
+    public static final String PARAM_ID = "id";
+    public static final String PARAM_IGNORE_ROOT_IN_FIELD_NAMES = "stripRoot";
+    public static final String PARAM_FILE = "document";
 
 }
--- crafter-search-server/src/main/java/org/craftercms/search/rest/controller/v1/SearchRestController.java
+++ crafter-search-server/src/main/java/org/craftercms/search/rest/controller/v1/SearchRestController.java
@@ -20,26 +20,19 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.craftercms.search.exception.SearchException;
 import org.craftercms.search.service.Query;
 import org.craftercms.search.service.SearchService;
-import org.craftercms.search.service.impl.QueryParams;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -48,6 +41,8 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import static org.craftercms.search.rest.v1.SearchRestApiConstants.*;
+
 /**
  * REST controller for the search service.
  *
@@ -55,27 +50,11 @@ import org.springframework.web.multipart.MultipartFile;
  * @author Dejan Brkic
  */
 @Controller
-@RequestMapping(SearchRestController.URL_ROOT)
+@RequestMapping(URL_ROOT)
 public class SearchRestController {
 
-    public static final String URL_ROOT = "/api/1/search";
-    public static final String URL_SEARCH = "/search";
-    public static final String URL_UPDATE = "/update";
-    public static final String URL_UPDATE_FILE= "/update-file";
-    public static final String URL_UPDATE_DOCUMENT = "/update-document";
-    public static final String URL_PARTIAL_DOCUMENT_UPDATE = "/partial-document-update";
-    public static final String URL_DELETE = "/delete";
-    public static final String URL_COMMIT = "/commit";
-
-    public static final String REQUEST_PARAM_INDEX_ID = "indexId";
-    public static final String REQUEST_PARAM_SITE = "site";
-    public static final String REQUEST_PARAM_ID = "id";
-    public static final String REQUEST_PARAM_IGNORE_ROOT_IN_FIELD_NAMES = "stripRoot";
-    public static final String REQUEST_PARAM_DOCUMENT = "document";
-    public static final String REQUEST_PARAM_FILE = "document";
-
     private static final String[] NON_ADDITIONAL_FIELD_NAMES =
-        {REQUEST_PARAM_INDEX_ID, REQUEST_PARAM_SITE, REQUEST_PARAM_ID, REQUEST_PARAM_DOCUMENT};
+        {PARAM_INDEX_ID, PARAM_SITE, PARAM_ID, PARAM_FILE};
 
     protected SearchService searchService;
     protected String multiValueSeparator;
@@ -99,7 +78,7 @@ public class SearchRestController {
     @RequestMapping(value = URL_SEARCH, method = RequestMethod.GET)
     @ResponseBody
     @SuppressWarnings("unchecked")
-    public Map<String, Object> search(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId,
+    public Map<String, Object> search(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId,
                                       HttpServletRequest request) throws SearchException {
         Query query = searchService.createQuery(request.getParameterMap());
 
@@ -108,10 +87,10 @@ public class SearchRestController {
 
     @RequestMapping(value = URL_UPDATE, method = RequestMethod.POST)
     @ResponseBody
-    public String update(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId,
-                         @RequestParam(REQUEST_PARAM_SITE) String site,
-                         @RequestParam(REQUEST_PARAM_ID) String id,
-                         @RequestParam(REQUEST_PARAM_IGNORE_ROOT_IN_FIELD_NAMES) boolean ignoreRootInFieldNames,
+    public String update(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId,
+                         @RequestParam(PARAM_SITE) String site,
+                         @RequestParam(PARAM_ID) String id,
+                         @RequestParam(PARAM_IGNORE_ROOT_IN_FIELD_NAMES) boolean ignoreRootInFieldNames,
                          @RequestBody String xml) throws SearchException {
         searchService.update(indexId, site, id, xml, ignoreRootInFieldNames);
 
@@ -120,9 +99,9 @@ public class SearchRestController {
 
     @RequestMapping(value = URL_DELETE, method = RequestMethod.POST)
     @ResponseBody
-    public String delete(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId,
-                         @RequestParam(REQUEST_PARAM_SITE) String site,
-                         @RequestParam(value = REQUEST_PARAM_ID) String id) throws SearchException {
+    public String delete(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId,
+                         @RequestParam(PARAM_SITE) String site,
+                         @RequestParam(value = PARAM_ID) String id) throws SearchException {
         searchService.delete(indexId, site, id);
 
         return getSuccessMessage("Delete of %s:%s for index %s successful", indexId, site, id);
@@ -130,7 +109,7 @@ public class SearchRestController {
 
     @RequestMapping(value = URL_COMMIT, method = RequestMethod.POST)
     @ResponseBody
-    public String commit(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId) throws SearchException {
+    public String commit(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId) throws SearchException {
         searchService.commit(indexId);
 
         return getSuccessMessage("Commit for index %s successful", indexId, null, null);
@@ -139,19 +118,19 @@ public class SearchRestController {
     @RequestMapping(value = {URL_UPDATE_DOCUMENT, URL_PARTIAL_DOCUMENT_UPDATE}, method = RequestMethod.POST)
     @ResponseBody
     @Deprecated
-    public String updateDocument(@RequestPart(REQUEST_PARAM_SITE) String site,
-                                 @RequestPart(REQUEST_PARAM_ID) String id,
-                                 @RequestPart(REQUEST_PARAM_DOCUMENT) MultipartFile document,
+    public String updateDocument(@RequestPart(PARAM_SITE) String site,
+                                 @RequestPart(PARAM_ID) String id,
+                                 @RequestPart(PARAM_FILE) MultipartFile document,
                                  HttpServletRequest request) throws SearchException {
          return updateContent(null, site, id, document, request, true);
     }
 
     @RequestMapping(value = URL_UPDATE_FILE, method = RequestMethod.POST)
     @ResponseBody
-    public String updateFile(@RequestParam(value = REQUEST_PARAM_INDEX_ID, required = false) String indexId,
-                             @RequestPart(REQUEST_PARAM_SITE) String site,
-                             @RequestPart(REQUEST_PARAM_ID) String id,
-                             @RequestPart(REQUEST_PARAM_FILE) MultipartFile file,
+    public String updateFile(@RequestParam(value = PARAM_INDEX_ID, required = false) String indexId,
+                             @RequestPart(PARAM_SITE) String site,
+                             @RequestPart(PARAM_ID) String id,
+                             @RequestPart(PARAM_FILE) MultipartFile file,
                              HttpServletRequest request) throws SearchException {
         return updateContent(indexId, site, id, file, request, false);
     }
