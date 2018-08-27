14e65b2b532becf226438ab77ba1195256ff583d
Jose Ross <jdross17@gmail.com>
2018-08-27 09:33:15 -0600

    Added new exception for specific server issues

---
+++ crafter-search-api/src/main/java/org/craftercms/search/exception/SearchServerException.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2007-2018 Crafter Software Corporation.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.craftercms.search.exception;
+
+/**
+ * Exception thrown when the search engine is not available.
+ * @author joseross
+ */
+public class SearchServerException extends SearchException {
+
+    public SearchServerException(final String msg) {
+        super(msg);
+    }
+
+    public SearchServerException(final String msg, final Throwable cause) {
+        super(msg, cause);
+    }
+
+    public SearchServerException(final String indexId, final String msg) {
+        super(indexId, msg);
+    }
+
+    public SearchServerException(final String indexId, final String msg, final Throwable throwable) {
+        super(indexId, msg, throwable);
+    }
+
+}
