5052bc065fcffa75f8d8a69f0443473ac1afe16b
Jose Ross <jdross17@gmail.com>
2019-03-01 16:42:43 -0600

    Upgrade Solr to v7.7.1: - Updated dependencies - Updated solr schema & configuration - Updated tests

--- crafter-search-batch-indexer/src/test/java/org/craftercms/search/batch/impl/XmlFileBatchIndexerTest.java
+++ crafter-search-batch-indexer/src/test/java/org/craftercms/search/batch/impl/XmlFileBatchIndexerTest.java
@@ -47,7 +47,22 @@ public class XmlFileBatchIndexerTest extends BatchIndexerTestBase {
     private static final String UPDATE_FILENAME = "test2.xml";
     private static final String DELETE_FILENAME = "deleteme.xml";
     private static final String EXPECTED_XML =  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                                                "<page>" +
+                                                "<!--\n" +
+                                                "  ~ Copyright (C) 2007-2019 Crafter Software Corporation. All Rights Reserved.\n" +
+                                                "  ~\n" +
+                                                "  ~ This program is free software: you can redistribute it and/or modify\n" +
+                                                "  ~ it under the terms of the GNU General Public License as published by\n" +
+                                                "  ~ the Free Software Foundation, either version 3 of the License, or\n" +
+                                                "  ~ (at your option) any later version.\n" +
+                                                "  ~\n" +
+                                                "  ~ This program is distributed in the hope that it will be useful,\n" +
+                                                "  ~ but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
+                                                "  ~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
+                                                "  ~ GNU General Public License for more details.\n" +
+                                                "  ~\n" +
+                                                "  ~ You should have received a copy of the GNU General Public License\n" +
+                                                "  ~ along with this program.  If not, see <http://www.gnu.org/licenses/>.\n" +
+                                                "  --><page>" +
                                                     "<fileName>test2.xml</fileName>" +
                                                     "<title>Test</title>" +
                                                     "<date format=\"MM/DD/YYY HH:MM:SS\">11/10/2015 00:00:00</date>" +
