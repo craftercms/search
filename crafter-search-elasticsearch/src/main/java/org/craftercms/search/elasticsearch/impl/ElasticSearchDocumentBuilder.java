150116f2b69b90cb34f86e0556ea677eb5129369
Jose Ross <jdross17@gmail.com>
2019-03-12 14:13:53 -0600

    Add unchanged copy for _html fields

--- crafter-search-elasticsearch/src/main/java/org/craftercms/search/elasticsearch/impl/ElasticSearchDocumentBuilder.java
+++ crafter-search-elasticsearch/src/main/java/org/craftercms/search/elasticsearch/impl/ElasticSearchDocumentBuilder.java
@@ -20,12 +20,16 @@ package org.craftercms.search.elasticsearch.impl;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 import org.apache.commons.collections.MapUtils;
 import org.craftercms.search.elasticsearch.jackson.MixedMultivaluedMap;
 import org.craftercms.search.exception.SearchException;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.util.MultiValueMap;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -38,9 +42,28 @@ public class ElasticSearchDocumentBuilder extends AbstractDocumentBuilder<Map<St
 
     protected ObjectMapper objectMapper;
 
+    /**
+     * Mapping of fields that require a copy before indexing
+     */
+    protected Map<String, String> copyFields;
+
+    /**
+     * Pattern for fields containing HTML markup
+     */
+    protected String markupFieldPattern;
+
     @Required
     public void setObjectMapper(final ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
+    }
+
+    public void setCopyFields(final Map<String, String> copyFields) {
+        this.copyFields = copyFields;
+    }
+
+    @Required
+    public void setMarkupFieldPattern(final String markupFieldPattern) {
+        this.markupFieldPattern = markupFieldPattern;
     }
 
     /**
@@ -53,6 +76,10 @@ public class ElasticSearchDocumentBuilder extends AbstractDocumentBuilder<Map<St
         try {
             Map<String, Object> map = objectMapper.readValue(xml, MixedMultivaluedMap.class);
             addFields(map, site, id, additionalFields);
+            if(MapUtils.isNotEmpty(copyFields)) {
+                addCopyFields(map);
+            }
+            updateFields(map);
             return map;
         } catch (Exception e) {
             throw new SearchException("Error building json for document " + id, e);
@@ -74,6 +101,60 @@ public class ElasticSearchDocumentBuilder extends AbstractDocumentBuilder<Map<St
         if(MapUtils.isNotEmpty(additionalFields)) {
             additionalFields.forEach((key, values) -> map.put(key, values.size() == 1? values.get(0) : values));
         }
+    }
+
+    /**
+     * Iterates recursively the given map to apply updates on single value nodes
+     * @param map the map to iterate
+     * @param consumer the update to apply
+     */
+    @SuppressWarnings("unchecked")
+    protected void updateMap(Map<String, Object> map,
+                             BiConsumer<Map.Entry<String, Object>, Map<String, Object>> consumer) {
+        Map<String, Object> temp = new HashMap<>();
+        map.entrySet().forEach(entry -> {
+            if(entry.getValue() instanceof Map) {
+                updateMap((Map<String, Object>)entry.getValue(), consumer);
+            } else if(entry.getValue() instanceof List) {
+                List list = (List) entry.getValue();
+                list.forEach(item -> {
+                    if(item instanceof Map) {
+                        updateMap((Map<String, Object>) item, consumer);
+                    }
+                });
+            } else {
+                consumer.accept(entry, temp);
+            }
+        });
+        if(MapUtils.isNotEmpty(temp)) {
+            map.putAll(temp);
+        }
+    }
+
+    /**
+     * Creates copies of all fields according to the mapping
+     * @param map the map to update
+     */
+    protected void addCopyFields(Map<String, Object> map) {
+        updateMap(map, (entry, temp) -> {
+            copyFields.forEach((pattern, name) -> {
+                if(entry.getKey().matches(pattern)) {
+                    temp.put(entry.getKey() + name, entry.getValue());
+                }
+            });
+        });
+    }
+
+    /**
+     * Updates fields according to the configuration
+     * @param map the map to update
+     */
+    protected void updateFields(Map<String, Object> map) {
+        updateMap(map, (entry, temp) -> {
+            if(entry.getKey().matches(markupFieldPattern)) {
+                temp.put(entry.getKey(), Jsoup.clean(entry.getValue().toString(), Whitelist.none()));
+            }
+        });
     }
 
 }
