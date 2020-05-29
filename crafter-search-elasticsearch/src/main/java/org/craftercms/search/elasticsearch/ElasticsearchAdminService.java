34176423b8372e3abcafe983fd61a6c7795805fa
Jose Ross <jdross17@gmail.com>
2020-05-29 10:16:52 -0600

    Improvements for Elasticsearch: - Refactor to clarify when an alias is used instead of an index - Add support for recreating indexes

--- crafter-search-elasticsearch/src/main/java/org/craftercms/search/elasticsearch/ElasticsearchAdminService.java
+++ crafter-search-elasticsearch/src/main/java/org/craftercms/search/elasticsearch/ElasticsearchAdminService.java
@@ -21,22 +21,32 @@ import org.craftercms.search.elasticsearch.exception.ElasticsearchException;
 /**
  * Provides operations to manage indices in Elasticsearch
  * @author joseross
+ * @since 3.1.0
  */
 public interface ElasticsearchAdminService extends AutoCloseable {
 
     /**
      * Creates an index
-     * @param indexName the name of the index
-     * @param isAuthoring indicates if the index if for authoring
+     * @param aliasName the name of the alias
+     * @param isAuthoring indicates if the index is for authoring
      * @throws ElasticsearchException if there is any error during the operation
      */
-    void createIndex(String indexName, boolean isAuthoring) throws ElasticsearchException;
+    void createIndex(String aliasName, boolean isAuthoring) throws ElasticsearchException;
 
     /**
-     * Deletes an index
-     * @param indexName the name of the index
+     * Deletes all indexes assigned to the given alias
+     * @param aliasName the name of the alias
      * @throws ElasticsearchException if there is any error during the operation
      */
-    void deleteIndex(String indexName) throws ElasticsearchException;
+    void deleteIndexes(String aliasName) throws ElasticsearchException;
+
+    /**
+     * Recreates an existing index
+     *
+     * @param aliasName the name of the alias
+     * @param isAuthoring indicates if the index is for authoring
+     * @throws ElasticsearchException if there is any error during the operation
+     */
+    void recreateIndex(String aliasName, boolean isAuthoring) throws ElasticsearchException;
 
 }
