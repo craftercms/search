e4acb8b80009fe74001b44f26dd00b4a3acb62f3
Jose Ross <jdross17@gmail.com>
2020-06-11 14:36:21 -0600

    Improvements for Elasticsearch: - Move all client creation code to a single class - Add new optional parameters for the client

--- crafter-search-elasticsearch/src/main/java/org/craftercms/search/elasticsearch/spring/ElasticsearchClientFactory.java
+++ crafter-search-elasticsearch/src/main/java/org/craftercms/search/elasticsearch/spring/ElasticsearchClientFactory.java
@@ -24,9 +24,12 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.AbstractFactoryBean;
 
 /**
@@ -34,6 +37,8 @@ import org.springframework.beans.factory.config.AbstractFactoryBean;
  * @author joseross
  */
 public class ElasticsearchClientFactory extends AbstractFactoryBean<RestHighLevelClient> {
+
+    private static final Logger logger = LoggerFactory.getLogger(ElasticsearchClientFactory.class);
 
     /**
      * List of Elasticsearch urls
@@ -50,6 +55,21 @@ public class ElasticsearchClientFactory extends AbstractFactoryBean<RestHighLeve
      */
     protected String password;
 
+    /**
+     * The connection timeout in milliseconds
+     */
+    protected int connectTimeout = -1;
+
+    /**
+     * The socket timeout in milliseconds
+     */
+    protected int socketTimeout = -1;
+
+    /**
+     * The number of threads to use
+     */
+    protected int threadCount = -1;
+
     public ElasticsearchClientFactory(final String[] serverUrls) {
         this.serverUrls = serverUrls;
     }
@@ -62,17 +82,65 @@ public class ElasticsearchClientFactory extends AbstractFactoryBean<RestHighLeve
         this.password = password;
     }
 
-    @Override
-    protected RestHighLevelClient createInstance() {
+    public void setConnectTimeout(int connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public void setSocketTimeout(int socketTimeout) {
+        this.socketTimeout = socketTimeout;
+    }
+
+    public void setThreadCount(int threadCount) {
+        this.threadCount = threadCount;
+    }
+
+    public static RestHighLevelClient createClient(String[] serverUrls, String username, String password,
+                                                   int connectTimeout, int socketTimeout, int threadCount) {
+        logger.debug("Building client for urls: {}", (Object) serverUrls);
         HttpHost[] hosts = Stream.of(serverUrls).map(HttpHost::create).toArray(HttpHost[]::new);
         RestClientBuilder clientBuilder = RestClient.builder(hosts);
-        if (StringUtils.isNoneEmpty(username, password)) {
-            CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
-            clientBuilder
-                .setHttpClientConfigCallback(builder -> builder.setDefaultCredentialsProvider(credentialsProvider));
-        }
+        RestClientBuilder.RequestConfigCallback requestConfigCallback = builder -> {
+            if (connectTimeout >= 0) {
+                logger.debug("Using custom connect timeout: {}", connectTimeout);
+                builder.setConnectTimeout(connectTimeout);
+            } else {
+                logger.debug("Using default connect timeout");
+            }
+            if (socketTimeout >= 0) {
+                logger.debug("Using custom socket timeout: {}", socketTimeout);
+                builder.setSocketTimeout(socketTimeout);
+            } else {
+                logger.debug("Using default socket timeout");
+            }
+            return builder;
+        };
+        RestClientBuilder.HttpClientConfigCallback httpClientConfigCallback = builder -> {
+            if (StringUtils.isNoneEmpty(username, password)) {
+                logger.debug("Using basic auth with user: {}", username);
+                CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+                credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+                builder.setDefaultCredentialsProvider(credentialsProvider);
+            } else {
+                logger.debug("No credentials provided");
+            }
+            if (threadCount > 0) {
+                logger.debug("Using custom thread count: {}", threadCount);
+                builder.setDefaultIOReactorConfig(IOReactorConfig.custom()
+                        .setIoThreadCount(threadCount)
+                        .build());
+            } else {
+                logger.debug("Using default thread count");
+            }
+            return builder;
+        };
+        clientBuilder.setRequestConfigCallback(requestConfigCallback);
+        clientBuilder.setHttpClientConfigCallback(httpClientConfigCallback);
         return new RestHighLevelClient(clientBuilder);
+    }
+
+    @Override
+    protected RestHighLevelClient createInstance() {
+        return createClient(serverUrls, username, password, connectTimeout, socketTimeout, threadCount);
     }
 
     @Override
