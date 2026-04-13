71ed00eda9707c37637657b461e07151fcbb991f
Jonathan Méndez <jonnathan.mendez@gmail.com>
2026-04-13 08:59:38 -0600

    Upgrade to Opensearch 3.6 (#353)

--- crafter-search-opensearch/src/main/java/org/craftercms/search/opensearch/impl/client/AbstractOpenSearchClientWrapper.java
+++ crafter-search-opensearch/src/main/java/org/craftercms/search/opensearch/impl/client/AbstractOpenSearchClientWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2026 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -283,7 +283,6 @@ public abstract class AbstractOpenSearchClientWrapper implements OpenSearchClien
             requestCache(request.requestCache());
             rescore(request.rescore());
             routing(request.routing());
-            runtimeMappings(request.runtimeMappings());
             scriptFields(request.scriptFields());
             scroll(request.scroll());
             if (CollectionUtils.isNotEmpty(request.searchAfter())) {
